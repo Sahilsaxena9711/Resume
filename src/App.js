c3b99cf3f6edf23c9f790f6abaabe8d65b282025
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2018-09-18 16:25:41 +0530

    resume added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import resume from './SahilSaxena.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span className="glyphicon glyphicon-name icon"></span>
+        <img src={resume} className="resume"/>
       </div>
     );
   }
